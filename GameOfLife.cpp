bec4f5928475e1ecbea8c9cd540ebf7137ace7e5
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2022-06-09 00:13:25 -0500

    Add project files

---
+++ GameOfLife.cpp
@@ -0,0 +1,32 @@
+﻿// GameOfLife.cpp : Defines the entry point for the application.
+//
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <array>
+
+template <int rows, int cols>
+struct GameState {
+    friend constexpr auto operator==(const GameState& lhs, const GameState& rhs) {
+        return lhs.grid == rhs.grid;
+    }
+
+    std::array<bool, rows* cols> grid{};
+};
+
+template <GameState gameState, int steps>
+struct NextState {
+    static constexpr auto value = gameState;
+};
+
+TEST_CASE("Game state generated") {
+    constexpr GameState<3, 3> initialState{
+        true, false, false,
+        false, false, false,
+        false, false, false,
+    };
+
+    static constexpr auto finalState = NextState<initialState, 0>::value;
+
+    REQUIRE(initialState == finalState);
+}
