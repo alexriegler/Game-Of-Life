59cb2884d7c9f732ab503f961c6c209acfceb30f
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2022-06-09 00:45:58 -0500

    Add 'Default game state' test

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -3,6 +3,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
 #include <array>
 
 template <int rows, int cols>
@@ -14,19 +15,31 @@ struct GameState {
     std::array<bool, rows* cols> grid{};
 };
 
-template <GameState gameState, int steps>
+template <GameState game_state, int steps>
 struct NextState {
-    static constexpr auto value = gameState;
+    static constexpr auto value = game_state;
 };
 
+// Variable template
+template <GameState game_state, int steps>
+inline constexpr auto NextState_v = NextState<game_state, steps>::value;
+
 TEST_CASE("Game state generated") {
-    constexpr GameState<3, 3> initialState{
-        true, false, false,
-        false, false, false,
-        false, false, false,
-    };
+    static constexpr GameState<3, 3> initial_state{};
 
-    static constexpr auto finalState = NextState<initialState, 0>::value;
+    SECTION("Standard") {
+        static constexpr auto final_state = NextState<initial_state, 0>::value;
+        REQUIRE(initial_state == final_state);
+    }
 
-    REQUIRE(initialState == finalState);
+    SECTION("Variable template") {
+        static constexpr auto finalState = NextState_v<initial_state, 0>;
+        REQUIRE(initial_state == finalState);
+    }
+}
+
+TEST_CASE("Default game state") {
+    static constexpr GameState<3, 3> state{};
+    constexpr auto is_false = [](bool b) { return !b; };
+    REQUIRE(std::ranges::all_of(state.grid, is_false));
 }
