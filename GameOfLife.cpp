f6a3839c824a36090084ecddece6f05319531522
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2022-06-25 19:31:34 -0500

    Add 'wraparound north' test

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -5,12 +5,20 @@
 
 #include <algorithm>
 #include <array>
+#include <iostream>
 
 namespace ranges = std::ranges;
 
 // Coordinates
 struct Coordinates {
     constexpr auto operator<=>(const Coordinates&) const = default;
+
+    // Used for debugging purposes
+    friend std::ostream& operator<<(std::ostream& os, const Coordinates& coords)
+    {
+        os << '(' << coords.row << ", " << coords.col << ')';
+        return os;
+    }
 
     int row{};
     int col{};
@@ -24,11 +32,13 @@ struct GameState {
     std::array<bool, rows* cols> grid{};
 };
 
+// TODO: Make this a template
 template <template <int, int> typename T, int rows, int cols>
 constexpr auto Rows(const T<rows, cols>&) {
     return rows;
 }
 
+// TODO: Make this a template
 template <template <int, int> typename T, int rows, int cols>
 constexpr auto Cols(const T<rows, cols>&) {
     return cols;
@@ -60,13 +70,56 @@ template <GameState game_state, int steps>
 inline constexpr auto NextState_v = NextState<game_state, steps>::value;
 
 // Adjacents
+using AdjacentCoords = std::array<Coordinates, 8>;
+
+template<GameState game_state, int row>
+struct RowWrap {
+    static constexpr auto value = row >= 0 ? row : Rows(game_state) - 1;
+};
+
+template <GameState game_state, int row>
+inline constexpr auto RowWrap_v = RowWrap<game_state, row>::value;
+
+template<GameState game_state, Coordinates coords>
+struct TopLeft {
+    static constexpr auto value = Coordinates {
+        RowWrap_v<game_state, coords.row - 1>,
+        coords.col - 1
+    };
+};
+
+template <GameState game_state, Coordinates coords>
+inline constexpr auto TopLeft_v = TopLeft<game_state, coords>::value;
+
+template<GameState game_state, Coordinates coords>
+struct TopMiddle {
+    static constexpr auto value = Coordinates{
+        RowWrap_v<game_state, coords.row - 1>,
+        coords.col
+    };
+};
+
+template <GameState game_state, Coordinates coords>
+inline constexpr auto TopMiddle_v = TopMiddle<game_state, coords>::value;
+
+template<GameState game_state, Coordinates coords>
+struct TopRight {
+    static constexpr auto value = Coordinates{
+        RowWrap_v<game_state, coords.row - 1>,
+        coords.col + 1
+    };
+};
+
+template <GameState game_state, Coordinates coords>
+inline constexpr auto TopRight_v = TopRight<game_state, coords>::value;
+
 template<GameState game_state, Coordinates coords>
 struct Adjacents {
-    static constexpr auto value = std::array<Coordinates, 8>{ {
-        { coords.row - 1, coords.col - 1 }, { coords.row - 1, coords.col }, { coords.row - 1, coords.col + 1 },
+    static constexpr auto value = AdjacentCoords{ {
+        TopLeft_v<game_state, coords>, TopMiddle_v<game_state, coords>, TopRight_v<game_state, coords>,
         { coords.row, coords.col - 1 },   /*{ coords.row, coords.col },   */{ coords.row, coords.col + 1 },
         { coords.row + 1, coords.col - 1 }, { coords.row + 1, coords.col }, { coords.row + 1, coords.col + 1 },
-        }};
+    }};
 };
 
 // Variable helper template
@@ -151,13 +204,31 @@ TEST_CASE("4x4, 1 alive, 1 step") {
 
 TEST_CASE("Get adjacent neighbors") {
     static constexpr GameState<3, 3> state{};
-    static constinit auto expected_coords = std::array<Coordinates, 8>{ {
-        { 0, 0 }, { 0, 1 }, { 0, 2 },
-        { 1, 0 }, /*{1, 1}*/{ 1, 2 },
-        { 2, 0 }, { 2, 1 }, { 2, 2 }
+
+    SECTION("(1, 1)") {
+        static constexpr auto center = Coordinates{ 1, 1 };
+        static constinit auto expected_coords = AdjacentCoords{ {
+            { 0, 0 }, { 0, 1 }, { 0, 2 },
+            { 1, 0 }, /*{1, 1}*/{ 1, 2 },
+            { 2, 0 }, { 2, 1 }, { 2, 2 }
         }};
-    static constinit auto actual_coords = Adjacents_v < state, Coordinates{ 1, 1 } > ;
-    ranges::sort(expected_coords);
-    ranges::sort(actual_coords);
-    REQUIRE(expected_coords == actual_coords);
+        static constinit auto actual_coords = Adjacents_v<state, center>;
+
+        ranges::sort(expected_coords);
+        ranges::sort(actual_coords);
+        REQUIRE(expected_coords == actual_coords);
+    }
+    SECTION("(0, 1) [wraparound north]") {
+        static constexpr auto center = Coordinates{ 0, 1 };
+        static constinit auto expected_coords = AdjacentCoords{ {
+            { 2, 0 }, { 2, 1 }, { 2, 2 },
+            { 0, 0 }, /*{0, 1}*/{ 0, 2 },
+            { 1, 0 }, { 1, 1 }, { 1, 2 }
+        } };
+        static constinit auto actual_coords = Adjacents_v<state, center>;
+
+        ranges::sort(expected_coords);
+        ranges::sort(actual_coords);
+        REQUIRE(expected_coords == actual_coords);
+    }
 }
