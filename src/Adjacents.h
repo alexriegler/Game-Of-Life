0cf818879e9521540d6b4b97688d9f811b4687c0
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2022-07-03 00:40:21 -0500

    Add 'wraparound south' test

--- src/Adjacents.h
+++ src/Adjacents.h
@@ -9,7 +9,12 @@ using AdjacentCoords = std::array<Coordinates, 8>;
 
 template<GameState game_state, int row>
 struct RowWrap {
-    static constexpr auto value = row >= 0 ? row : Rows(game_state) - 1;
+    static constexpr auto value =
+        row >= 0 ?
+            ( row < Rows(game_state) ?
+                row
+                : 0 )
+            : Rows(game_state) - 1;
 };
 
 template <GameState game_state, int row>
@@ -49,11 +54,44 @@ template <GameState game_state, Coordinates coords>
 inline constexpr auto TopRight_v = TopRight<game_state, coords>::value;
 
 template<GameState game_state, Coordinates coords>
+struct BottomLeft {
+    static constexpr auto value = Coordinates{
+        RowWrap_v<game_state, coords.row + 1>,
+        coords.col - 1
+    };
+};
+
+template <GameState game_state, Coordinates coords>
+inline constexpr auto BottomLeft_v = BottomLeft<game_state, coords>::value;
+
+template<GameState game_state, Coordinates coords>
+struct BottomMiddle {
+    static constexpr auto value = Coordinates{
+        RowWrap_v<game_state, coords.row + 1>,
+        coords.col
+    };
+};
+
+template <GameState game_state, Coordinates coords>
+inline constexpr auto BottomMiddle_v = BottomMiddle<game_state, coords>::value;
+
+template<GameState game_state, Coordinates coords>
+struct BottomRight {
+    static constexpr auto value = Coordinates{
+        RowWrap_v<game_state, coords.row + 1>,
+        coords.col + 1
+    };
+};
+
+template <GameState game_state, Coordinates coords>
+inline constexpr auto BottomRight_v = BottomRight<game_state, coords>::value;
+
+template<GameState game_state, Coordinates coords>
 struct Adjacents {
     static constexpr auto value = AdjacentCoords{ {
         TopLeft_v<game_state, coords>, TopMiddle_v<game_state, coords>, TopRight_v<game_state, coords>,
         { coords.row, coords.col - 1 },   /*{ coords.row, coords.col },   */{ coords.row, coords.col + 1 },
-        { coords.row + 1, coords.col - 1 }, { coords.row + 1, coords.col }, { coords.row + 1, coords.col + 1 },
+        BottomLeft_v<game_state, coords>, BottomMiddle_v<game_state, coords>, BottomRight_v<game_state, coords>,
     } };
 };
 
--- src/GameOfLife.cpp
+++ src/GameOfLife.cpp
@@ -82,7 +82,7 @@ TEST_CASE("4x4, 1 alive, 1 step") {
 //    }
 //}
 
-TEST_CASE("Get adjacent neighbors") {
+TEST_CASE("Get adjacent neighbors [3x3]") {
     static constexpr GameState<3, 3> state{};
 
     SECTION("(1, 1)") {
@@ -111,4 +111,24 @@ TEST_CASE("Get adjacent neighbors") {
         ranges::sort(actual_coords);
         REQUIRE(expected_coords == actual_coords);
     }
+    SECTION("(2, 1) [wraparound south]") {
+        static constexpr auto center = Coordinates{ 2, 1 };
+        static constinit auto expected_coords = AdjacentCoords{ {
+            { 1, 0 }, { 1, 1 }, { 1, 2 },
+            { 2, 0 }, /*{2, 1}*/{ 2, 2 },
+            { 0, 0 }, { 0, 1 }, { 0, 2 }
+        } };
+        static constinit auto actual_coords = Adjacents_v<state, center>;
+
+        ranges::sort(expected_coords);
+        ranges::sort(actual_coords);
+        REQUIRE(expected_coords == actual_coords);
+    }
+    // TODO: Add mores cases
+    // TODO: Rename directions to use cardinal directions (i.e., N, S, SE, etc.)
+    // TODO: Refactor row wrap to require just max rows and row. No depend on game state
+    // TODO: Add unit tests for row wrap
+    // TODO: Add unit tests for directions
+    // TODO: Add tests directory to project structure
+    // TODO: Add demo app for the game of life
 }
